202797bdac111b2557849969ab98db8fecbacaa2
Ian Huang (Shaoru) <ian1314159@gmail.com>
2024-04-03 15:46:13 -0700

    add rules

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+  // RULES OF CONWAY'S GAME OF LIFE
+  // Cell states: alive or dead. Game progresses in generations.
+  // Next generation cell state depends on eight neighbors:
+  //   - Live cell < 2 neighbors dies (underpopulation).
+  //   - Live cell with 2 or 3 neighbors survives.
+  //   - Live cell > 3 neighbors dies (overpopulation).
+  //   - Dead cell with 3 neighbors revives (reproduction).
 </script>
 
 <main></main>
